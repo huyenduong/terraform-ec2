e982332911fd9466fe967dd70ec84bd6c18774e1
Huyen Duong <duongtrunghuyen@gmail.com>
2019-06-13 19:04:35 +0800

    Update terraform 0.12

--- instance.tf
+++ instance.tf
@@ -3,15 +3,15 @@ resource "aws_instance" "instance01" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
   key_name = "sydneyhtduong01"
-  tags {
-    Name = "terra_web01"
+  tags = {
+    Name = "terra_app01"
     Use = "Demo for ACI Anywhere with Terraform"
   }
 
   # the VPC subnet
-  subnet_id = "subnet-024a9101f7b61bf63"
+  subnet_id = "subnet-0f17ee7d404cd928c"
   # set private ip address
-  private_ip ="172.11.1.101"
+  # private_ip ="172.11.1.101"
   associate_public_ip_address = true
 }
 
@@ -20,14 +20,67 @@ resource "aws_instance" "instance02" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
   key_name = "sydneyhtduong01"
-  tags {
-    Name = "terra_web02"
+  tags = {
+    Name = "terra_app02"
     Use = "Demo for ACI Anywhere with Terraform"
   }
 
   # the VPC subnet
-  subnet_id = "subnet-024a9101f7b61bf63"
+  subnet_id = "subnet-0f17ee7d404cd928c"
   # set private ip address
-  private_ip ="172.11.1.102"
+  #private_ip ="172.11.1.102"
+  associate_public_ip_address = true
+}
+
+
+# Instance 03
+resource "aws_instance" "instance03" {
+  ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type = "t2.micro"
+  key_name = "sydneyhtduong01"
+  tags = {
+    Name = "terra_app03"
+    Use = "Demo for ACI Anywhere with Terraform"
+  }
+
+  # the VPC subnet
+  subnet_id = "subnet-0f17ee7d404cd928c"
+  # set private ip address
+  #private_ip ="172.11.1.102"
+  associate_public_ip_address = true
+}
+
+
+# Instance 04
+resource "aws_instance" "instance04" {
+  ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type = "t2.micro"
+  key_name = "sydneyhtduong01"
+  tags = {
+    Name = "terra_app04"
+    Use = "Demo for ACI Anywhere with Terraform"
+  }
+
+  # the VPC subnet
+  subnet_id = "subnet-0f17ee7d404cd928c"
+  # set private ip address
+  #private_ip ="172.11.1.102"
+  associate_public_ip_address = true
+}
+
+# Instance 05
+resource "aws_instance" "instance05" {
+  ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type = "t2.micro"
+  key_name = "sydneyhtduong01"
+  tags = {
+    Name = "terra_app05"
+    Use = "Demo for ACI Anywhere with Terraform"
+  }
+
+  # the VPC subnet
+  subnet_id = "subnet-0f17ee7d404cd928c"
+  # set private ip address
+  #private_ip ="172.11.1.102"
   associate_public_ip_address = true
 }
